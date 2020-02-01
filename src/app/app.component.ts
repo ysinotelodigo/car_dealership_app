20084f3a6f028da0750cf6d5e747f454c6887bec
ysinotelodigo <ysinotelodigo@gmail.com>
2020-02-01 02:20:29 +0100

    Add click functions for getSale and removeCar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,23 @@ export class AppComponent  implements OnInit{
   
   private available: Array<Car> = [];
 
-    
-
+  
   ngOnInit() {
     this.available.push(new Car("scenic.png", "Renault","Scenic", new Date(2007,1,1),new Date(2018,4,1),5000,""));
     this.available.push(new Car("ibiza.png", "Seat","Ibiza", new Date(2003,1,1),new Date(2018,3,1),1200,""));
     this.available.push(new Car("megane.png", "Renault","Megane", new Date(2007,1,1),new Date(2018,3,1),3500,""));
     this.available.push(new Car("model3.png", "Tesla","Model3", new Date(2007,1,1),new Date(2018,3,1),4000,""));
     this.available.push(new Car("laguna.png", "Renault","Laguna", new Date(2006,1,1),new Date(2018,2,1),2500,""));
+  }
+
+  public removeCar(car : Car):void{
+    const index = this.available.indexOf(car, 0);
+    if (index > -1) {
+      this.available.splice(index, 1);
+    }
+  }
+
+  public getSale(car : Car):void{
+    car.price *= 0.9;
   }
 }
